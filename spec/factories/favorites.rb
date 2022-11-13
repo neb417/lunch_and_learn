4e36625da030b0ccc23a0577f33e886e1b29fc68
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-11-13 16:42:29 -0700

    add favorites to database. verify relationship with user. setup factorybot.

---
+++ spec/factories/favorites.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :favorite do
+    user
+    country { Faker::Address.country }
+    recipe_link { Faker::Internet.url }
+    recipe_title { Faker::Food.dish }
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'user validations' do
     it { should validate_uniqueness_of(:email) }
+    it { should have_many :favorites }
   end
 end
