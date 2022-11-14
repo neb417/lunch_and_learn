1bf3cd32bc4b71fe101ccf39057a491f56ecbffb
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-11-14 11:33:10 -0700

    refactor TouristSitesController.index

--- app/controllers/api/v1/tourist_sights_controller.rb
+++ app/controllers/api/v1/tourist_sights_controller.rb
@@ -2,6 +2,10 @@ class Api::V1::TouristSightsController < ApplicationController
   def index
     country = params[:country] || CountryFacade.select_country
     latlng = CountryFacade.return_capital_latlng(country)
+    return_sites(country, latlng)
+  end
+
+  def return_sites(country, latlng)
     if latlng.include?(:status)
       render json: PlaceSerializer.error_serializer(country, latlng), status: latlng[:status]
     else
