705577708e2556e1bea4495a1b4eff61c8d4729b
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-11-14 13:38:09 -0700

    refactor users controller.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,19 +1,24 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :new_user
   def create
-    user = User.new(user_params)
+    return create_user if @user.save
 
-    if user.save
-      params[:api_key] = user.generate_api
-      user.update(user_params)
-      render json: UserSerializer.serialize_user(user), status: 201
-    else
-      render json: UserSerializer.error_serializer(user.errors.full_messages), status: 400
-    end
+    render json: UserSerializer.error_serializer(@user.errors.full_messages), status: 400
   end
 
   private
 
   def user_params
     params.permit(:name, :email, :api_key)
+  end
+
+  def new_user
+    @user = User.new(user_params)
+  end
+
+  def create_user
+    params[:api_key] = @user.generate_api
+    @user.update(user_params)
+    render json: UserSerializer.serialize_user(@user), status: 201
   end
 end
