fb5f9a52fd23edb2744d8b3e31b7d41ecf9722ae
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-11-14 13:46:48 -0700

    refactor learning resources controller.

--- app/controllers/api/v1/learning_resources_controller.rb
+++ app/controllers/api/v1/learning_resources_controller.rb
@@ -1,8 +1,14 @@
 class Api::V1::LearningResourcesController < ApplicationController
+  before_action :country
   def index
-    country = params[:country]
-    video_title, video_id = VideoFacade.return_video(country)
-    images = ImageFacade.return_images(country)
-    render json: LearningResourceSerializer.serialze_media(country, video_title, video_id, images)
+    video_title, video_id = VideoFacade.return_video(@country)
+    images = ImageFacade.return_images(@country)
+    render json: LearningResourceSerializer.serialze_media(@country, video_title, video_id, images)
+  end
+
+  private
+
+  def country
+    @country ||= params[:country]
   end
 end
